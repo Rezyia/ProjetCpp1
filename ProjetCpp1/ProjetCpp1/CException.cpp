4ed3697ef644e6aa6be224502f27b88d825dd760
Okaazkul <dylan.vinet29@gmail.com>
2021-04-09 11:05:01 +0200

    création CException

---
+++ ProjetCpp1/ProjetCpp1/CException.cpp
@@ -0,0 +1,21 @@
+#include "CException.h"
+#include <cstring>
+
+CException::CException(char * pcErr)
+{
+	strcpy(pcErreur, pcErr);
+}
+
+CException::CException(CException * EXCcopy)
+{
+	strcpy(pcErreur, EXCcopy->EXCLireErreur());
+}
+
+CException::~CException()
+{
+}
+
+char* CException::EXCLireErreur()
+{
+	return pcErreur;
+}
