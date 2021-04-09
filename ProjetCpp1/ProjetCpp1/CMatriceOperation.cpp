33efe7814d75f1b5a4ce539d6870a1f54f3618a4
Rezyia <fineladiesprotectress@gmail.com>
2021-04-09 11:40:37 +0200

    maj multiplication de 2 matrices

--- ProjetCpp1/ProjetCpp1/CMatriceOperation.cpp
+++ ProjetCpp1/ProjetCpp1/CMatriceOperation.cpp
@@ -66,19 +66,21 @@ CMatriceOperation<Type> CMatriceOperation<Type>::operator-(CMatriceOperation<Typ
 template<class Type>
 CMatriceOperation<Type> CMatriceOperation<Type>::operator*(CMatriceOperation<Type> MAOArg)
 {
-	if (this->MATLireNbreLignes() != MAOArg->MATLireNbreColonnes()) {
+	if (MAOArg->MATLireNbreLignes() != this->MATLireNbreColonnes()) {
 		std::cout << "Erreur : Les matrices ne peuvent pas être multipliées entre elles.\n";
 	}
 	else {
-		unsigned int nbCol = MAOarg->MATLireNbreColonnes(), nbRow = this->MATLireNbreLignes(); // hauteur MatA et longueur MatB de MatA*MatB
+		unsigned int nbRow = this->MATLireNbreLignes(), nbCol = MAOArg->MATLireNbreColonnes(); // hauteur MatA et longueur MatB de MatA*MatB
 		CMatriceOperation<Type> MAOresult = new CMatriceOperation<Type>(nbCol, nbRow);
 		Type calcul;
 
 		for (int i = 0; i < nbRow; i++) {
 			for (int j = 0; j < nbCol; j++) {
 				calcul = 0;
-				for (int k=0; k<this->MATLireNbreLignes())
-				MAOresult->MATModifierVal(this->MATLireVal(i, j) + MAOArg->MATLireVal(i, j), i, j);
+				for (int k = 0; k < this->MATLireNbreColonnes(); k++) {
+					calcul += this->MATLireVal(i, k) * MAOArg->MATLireVal(k, j);
+				}
+				MAOresult->MATModifierVal(calcul, i, j);
 			}
 		}
 		return MAOresult;
